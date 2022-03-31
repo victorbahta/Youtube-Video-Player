c822c2dd621afdf5e9e5f66647c1ef473bb24fe7
victorbahta <44603464+victorbahta@users.noreply.github.com>
2022-03-31 00:26:21 -0600

    Removed Api key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const KEY = "AIzaSyDHfapjoR4dNMGzixUWzYJEFFK8ySogxKE";
+const KEY = "";
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
